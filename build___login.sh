2499310b14e9a1e19004ee9f4d6be08e53f8e82d
dan1114ho <dan1114ho@gmail.com>
2018-10-04 17:55:39 -0600

    remember the user's previous drop down selections

---
+++ build___login.sh
@@ -0,0 +1,1 @@
+$(aws ecr get-login --no-include-email)
--- src/components/containers/Dashboard/DashboardContainer.ts
+++ src/components/containers/Dashboard/DashboardContainer.ts
@@ -1,9 +1,9 @@
 import Vue from 'vue';
-import { Component} from 'vue-property-decorator'
+import {Component, Watch} from 'vue-property-decorator'
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCheck } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {faCheck} from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 import {MutationTypes} from '../../../store/mutation-types';
 
 library.add(faCheck);
@@ -25,40 +25,46 @@ declare function getSubjectId();
      };
   },
 
-
   created() {
+
+    function formatOptions(corpora) {
+        const rv = [];
+        corpora.forEach(function(corpus) {
+            rv.push(
+              { 'value': corpus[0],  'text': corpus[1], }
+            );
+        });
+        return rv;
+    }
+
+    const localX = localStorage.getItem('adopt_curve_x');
+    if (localX) {
+      this.$data.adopt_curve_x = localX;
+    }
+    const localY = localStorage.getItem('adopt_curve_y');
+    if (localY) {
+      this.$data.adopt_curve_y = localY;
+    }
+
     this.$store.dispatch(MutationTypes.GET_CORPORA, { payload: {}, callback: res => {
-
-        const client_corp_formated = [];
-        res.data['client_corpora'].forEach(function(corpus) {
-            const elem = {
-              'value': corpus[0],
-              'text': corpus[1],
-            };
-            client_corp_formated.push(elem);
-        });
-
-        const common_corp_formated = [];
-        res.data['common_corpora'].forEach(function(corpus) {
-            const elem = {
-              'value': corpus[0],
-              'text': corpus[1],
-            };
-            common_corp_formated.push(elem);
-        });
-
-        this.$data.adopt_curve_x_options = common_corp_formated;
-        this.$data.adopt_curve_x = res.data['common_corpora'][0][0];
-
-        this.$data.adopt_curve_y_options = client_corp_formated;
-        this.$data.adopt_curve_y = res.data['client_corpora'][0][0];
-
+        this.$data.adopt_curve_x_options = formatOptions(res.data['common_corpora']);
+        this.$data.adopt_curve_y_options = formatOptions(res.data['client_corpora']);
+        if (!this.$data.adopt_curve_x) {
+          this.$data.adopt_curve_x = res.data['common_corpora'][0][0];
+        }
+        if (!this.$data.adopt_curve_y) {
+          this.$data.adopt_curve_y = res.data['client_corpora'][0][0];
+        }
     }});
-
   }
-
 })
 
 
 export class DashboardContainer extends Vue {
+  @Watch('adopt_curve_x') adoptCurveXChanged(newValue, oldValue) {
+    localStorage.setItem('adopt_curve_x', newValue)
+  }
+  @Watch('adopt_curve_y') adoptCurveYChanged(newValue, oldValue) {
+    localStorage.setItem('adopt_curve_y', newValue)
+  }
 }
