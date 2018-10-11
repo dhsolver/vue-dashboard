b9e7e25afeded7a161174037a7f834fbe78b39b1
dan1114ho <dan1114ho@gmail.com>
2018-10-11 02:31:27 -0600

    remove irrelevant charts. remove number in top rectangles. add drop-downs to Innovator.

--- src/containers/Dashboard/DashboardContainer.ts
+++ src/containers/Dashboard/DashboardContainer.ts
@@ -22,6 +22,10 @@ declare function getSubjectId();
        adopt_curve_y: '',
        adopt_curve_x_options: [],
        adopt_curve_y_options: [],
+       influencers_x: '',
+       influencers_y: '',
+       influencers_x_options: [],
+       influencers_y_options: [],
      };
   },
 
@@ -37,13 +41,22 @@ declare function getSubjectId();
         return rv;
     }
 
-    const localX = localStorage.getItem('adopt_curve_x');
-    if (localX) {
-      this.$data.adopt_curve_x = localX;
+    const adopCurveLocalX = localStorage.getItem('adopt_curve_x');
+    if (adopCurveLocalX) {
+      this.$data.adopt_curve_x = adopCurveLocalX;
     }
-    const localY = localStorage.getItem('adopt_curve_y');
-    if (localY) {
-      this.$data.adopt_curve_y = localY;
+    const adopCurveLocalY = localStorage.getItem('adopt_curve_y');
+    if (adopCurveLocalY) {
+      this.$data.adopt_curve_y = adopCurveLocalY;
+    }
+
+    const influencersLocalX = localStorage.getItem('influencers_x');
+    if (influencersLocalX) {
+      this.$data.influencers_x = influencersLocalX;
+    }
+    const influencersLocalY = localStorage.getItem('influencers_y');
+    if (influencersLocalY) {
+      this.$data.influencers_y = influencersLocalY;
     }
 
     this.$store.dispatch(MutationTypes.GET_CORPORA, { payload: {}, callback: res => {
@@ -55,16 +68,40 @@ declare function getSubjectId();
         if (!this.$data.adopt_curve_y) {
           this.$data.adopt_curve_y = res.data['client_corpora'][0][0];
         }
+
+        this.$data.influencers_x_options = formatOptions(res.data['common_corpora']);
+        this.$data.influencers_y_options = formatOptions(res.data['client_corpora']);
+        if (!this.$data.influencers_x) {
+          this.$data.influencers_x = res.data['common_corpora'][0][0];
+        }
+        if (!this.$data.influencers_y) {
+          this.$data.influencers_y = res.data['client_corpora'][0][0];
+        }
     }});
+
+
   }
 })
 
 
 export class DashboardContainer extends Vue {
-  @Watch('adopt_curve_x') adoptCurveXChanged(newValue, oldValue) {
+  @Watch('adopt_curve_x')
+  adoptCurveXChanged(newValue) {
     localStorage.setItem('adopt_curve_x', newValue)
   }
-  @Watch('adopt_curve_y') adoptCurveYChanged(newValue, oldValue) {
+
+  @Watch('adopt_curve_y')
+  adoptCurveYChanged(newValue) {
     localStorage.setItem('adopt_curve_y', newValue)
+  }
+
+  @Watch('influencers_x')
+  influencersXChanged(newValue) {
+    localStorage.setItem('influencers_x', newValue)
+  }
+
+  @Watch('influencers_y')
+  influencersYChanged(newValue) {
+    localStorage.setItem('influencers_y', newValue)
   }
 }
