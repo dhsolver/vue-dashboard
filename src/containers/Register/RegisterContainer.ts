94440f015dc4e4c8e1d3721b42a6029174f50285
Michael Buchko <webguru35@gmail.com>
2018-10-14 04:53:46 +0900

    update registration form

--- src/containers/Register/RegisterContainer.ts
+++ src/containers/Register/RegisterContainer.ts
@@ -4,7 +4,7 @@ import { Component } from 'vue-property-decorator'
 import './styles.scss';
 
 declare function registeringWithCode (code);
-declare function registeringRequest (email, pw);
+declare function registeringRequest (email, pw, fname, lname);
 
 @Component({
   template: require('./register.html'),
@@ -13,20 +13,30 @@ declare function registeringRequest (email, pw);
 
 })
 export class RegisterContainer extends Vue {
+  firstname: string = '';
+  firstnameValidated: boolean = true;
+  lastname: string = '';
+  lastnameValidated: boolean = true;
   email: string = '';
   emailValidated: boolean = true;
   password: string = '';
   passwordConfirm: string = '';
   passwordValidated: boolean = true;
+  agree: boolean = false;
+  agreeError: string = '';
 
   step: number = 0;
   code: string = '';
   codeValidated: boolean = true;
 
   userRegister() {
+    this.firstnameValidated = this.firstname ? true : false;
+    this.lastnameValidated = this.lastname ? true : false;
+    this.emailValidated = this.email ? true : false;
     this.passwordValidated = (this.passwordConfirm === this.password) && (this.password !== '');
-    if (this.passwordValidated) {
-      registeringRequest(this.email, this.password);
+    this.agreeError = this.agree ? '' : 'Please confirm that you agreed to our Terms and Policy';
+    if (this.firstnameValidated && this.lastnameValidated && this.emailValidated && this.passwordValidated && this.agree) {
+      registeringRequest(this.email, this.password, this.firstname, this.lastname);
     }
   }
 
