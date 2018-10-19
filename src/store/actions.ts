e49b79e585895038b11c7559d5b750e828939d76
dan1114ho <dan1114ho@gmail.com>
2018-10-19 20:46:10 +0200

    Integrate /stripe api

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -285,6 +285,22 @@ const actions: ActionTree<State, State> = {
           msg: 'Failed to get billing information.'
         });
       });
+  },
+
+  [MutationTypes.STRIPE_CHECKOUT]: ({ commit }, { payload, callback }) => {
+    sendPost('/stripe', payload)
+      .then((res: any) => {
+        callback({
+          status: 'ok',
+          data: res.data
+        });
+      })
+      .catch((error: any) => {
+        callback({
+          status: 'error',
+          msg: 'Failed to checkout.'
+        });
+      });
   }
 };
 
