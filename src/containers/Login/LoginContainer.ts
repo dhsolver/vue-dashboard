eac74ffdeb2c6e63aef0ff566edb3c72c662f1da
dan1114ho <dan1114ho@gmail.com>
2018-10-25 20:32:39 +0200

    Clean up login workflow

--- src/containers/Login/LoginContainer.ts
+++ src/containers/Login/LoginContainer.ts
@@ -1,20 +1,18 @@
 import Vue from 'vue';
 import { MutationTypes } from '../../store/mutation-types';
-import { Component, Prop, Watch } from 'vue-property-decorator'
-import { Getter } from 'vuex-class';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCheck } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import router from '../../router';
-import store from '../../store';
+import { Component, Prop, Watch } from 'vue-property-decorator';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import * as _ from 'lodash';
 
 import { Footbar } from '../../components/Footbar';
 
 import './styles.scss';
 
-declare function initializeStorage ();
+declare function clearStorage();
 
-library.add(faCheck)
+library.add(faCheck);
 
 @Component({
   template: require('./login.html'),
@@ -24,74 +22,63 @@ library.add(faCheck)
   }
 })
 export class LoginContainer extends Vue {
-  userName = '';
-  userNameValidated = true;
-  password = '';
-  passwordValidated = true;
-  error = '';
-  @Getter('loginStorage', {}) loginStorage!: any;
-  @Getter('loggedIn', {}) loggedIn!: any;
-  @Getter('loginError') loginError!: string;
+  userData: any = {};
+  error: any = {};
+  isBusy: boolean = false;
 
-  @Watch('loggedIn') loggedInChanged(value, oldValue) {
-    if (value && !oldValue) {
-      if (localStorage.getItem('firstLogin') == null) {
-        if (localStorage.getItem('accountInfo') != null) {
-          this.createAccount();
-        }
-        else {
-          this.getClientName('/dashboard');
-        }
-        localStorage.setItem('firstLogin', 'no');
-      }
-      else {
-        this.getClientName('/dashboard');
-        localStorage.setItem('firstLogin', 'no');
-      }
+  async login() {
+    // Form Validation
+    this.error = {};
+    if (!this.userData.username) this.error.username = 'Please input your username.';
+    if (!this.userData.password) this.error.password = 'Please input the password.';
+    if (!_.isEmpty(this.error)) return;
+    
+    // Login user
+    this.isBusy = true;
+    const loginUserResponse = await this.$store.dispatch(MutationTypes.LOGIN_USER_REQUEST, this.userData);
+    if (loginUserResponse.status === 'error') {
+      this.error.response = loginUserResponse.msg;
+      this.error = { ...this.error };
+      this.isBusy = false;
+      return;
     }
-  }
-  @Watch('loginError') loginErrorChanged(value, oldValue) {
-    this.error = value;
-  }
-  userLogin() {
-    this.userNameValidated = this.userName !== '';
-    this.passwordValidated = this.password !== '';
-    if (this.userNameValidated && this.passwordValidated) {
-      const loginInfo = { username: this.userName, password: this.password };
-      this.$store.dispatch(MutationTypes.LOGIN_USER, loginInfo);
-    }
-  }
-  userLogout() {
-    this.$store.dispatch(MutationTypes.LOGOUT_USER);
-  }
-
-  createAccount() {
-    const accountInfo = JSON.parse(localStorage.getItem('accountInfo'));
-    this.$store.dispatch(MutationTypes.CREATE_ACCOUNT, { payload: accountInfo, callback: (res) => {
-      if (res.status === 'ok') {
-        console.log('Account has been created successfully.');
+    if (localStorage.getItem('firstLogin') === null) {
+      const accountInfo = JSON.parse(localStorage.getItem('accountInfo'));
+      if (accountInfo) {
+        this.createAccount();
+      } else {
         this.getClientName('/create-campaign');
       }
-      else {
-        this.error = res.msg;
-        localStorage.clear();
-        initializeStorage();
-      }
-    }});
+      localStorage.setItem('firstLogin', 'no');
+    } else {
+      this.getClientName('/dashboard');
+    }
   }
 
-  getClientName(routeName) {
-    this.$store.dispatch(MutationTypes.GET_CLIENT_NAME_REQUEST, {
-      payload: {}, callback: (res) => {
-        if (res.status === 'ok') {
-          localStorage.setItem('clientName', res.msg);
-          router.push(routeName);
-        } else {
-          this.error = res.msg;
-          localStorage.clear();
-          initializeStorage();
-        }
-      }
-    });
+  async createAccount() {
+    const accountInfo = JSON.parse(localStorage.getItem('accountInfo'));
+    const createAccountResponse = await this.$store.dispatch(MutationTypes.CREATE_ACCOUNT_REQUEST, accountInfo);
+    if (createAccountResponse.status === 'error') {
+      this.error.response = createAccountResponse.msg;
+      this.error = { ...this.error };
+      clearStorage();
+      this.isBusy = false;
+      return;
+    }
+    this.getClientName('/create-campaign');
+  }
+
+  async getClientName(routeName) {
+    const getClientNameResponse = await this.$store.dispatch(MutationTypes.GET_CLIENT_NAME_REQUEST, {});
+    if (getClientNameResponse.status === 'error') {
+      this.error.response = getClientNameResponse.msg;
+      this.error = { ...this.error };
+      clearStorage();
+      this.isBusy = false;
+      return;
+    }
+    localStorage.setItem('clientName', getClientNameResponse.msg);
+    this.$router.replace(routeName);
+    this.isBusy = false;
   }
 }
