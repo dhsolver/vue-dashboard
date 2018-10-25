ea20826473f2a976345a7fa412588b17cb359547
dan1114ho <dan1114ho@gmail.com>
2018-10-25 19:42:50 +0200

    Clean up components

--- src/components/Footbar/Footbar.ts
+++ src/components/Footbar/Footbar.ts
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import { Component, Watch } from 'vue-property-decorator';
-import { Logger } from '../../utils/log';
 import { Getter } from 'vuex-class';
 
 import './style.scss';
@@ -9,22 +8,5 @@ import './style.scss';
   template: require('./Footbar.html')
 })
 export class Footbar extends Vue {
-
-  protected logger: Logger;
-
-
-  inverted: boolean = true; // default value
-
-  object: { default: string } = { default: 'Default object property!' }; // objects as default values don't need to be wrapped into functions
-
-  @Getter('loggedIn', {}) loggedIn!: any;
-
-  @Watch('$route.path')
-  pathChanged() {
-    this.logger.info('Changed current path to: ' + this.$route.path);
-  }
-  mounted() {
-    if (!this.logger) this.logger = new Logger();
-    this.$nextTick(() => this.logger.info(this.object.default));
-  }
+  @Getter('isLoggedIn') isLoggedIn!: boolean;
 }
