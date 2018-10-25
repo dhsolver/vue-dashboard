27164ca98bfcb8dc6e8bca6dc6f62f3cb8b3070b
dan1114ho <dan1114ho@gmail.com>
2018-10-25 20:36:21 +0200

    Clean up forgot password

--- src/containers/ForgotPassword/ForgotPasswordContainer.ts
+++ src/containers/ForgotPassword/ForgotPasswordContainer.ts
@@ -1,12 +1,10 @@
 import Vue from 'vue';
 import { MutationTypes } from '../../store/mutation-types';
 import { Component, Prop, Watch } from 'vue-property-decorator'
-import { Getter } from 'vuex-class';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faCheck } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import router from '../../router';
-import store from '../../store';
+import * as _ from 'lodash';
 
 import './styles.scss';
 
@@ -19,34 +17,52 @@ library.add(faCheck)
   }
 })
 export class ForgotPasswordContainer extends Vue {
-  username = '';
-  usernameValidated = true;
-  code = '';
-  codeValidated = true;
-  newPassword = '';
-  newPasswordValidated = true;
-  confirmPassword = '';
-  confirmPasswordValidated = true;
-  @Getter('forgotPasswordStatus') forgotPasswordStatus!: string;
-  @Getter('forgotPasswordError') forgotPasswordError!: string;
-  @Getter('confirmPasswordStatus') confirmPasswordStatus!: string;
-  @Getter('confirmPasswordError') confirmPasswordError!: string;
+  step: number = 0;
+  userData: any = {};
+  error: any = {};
+  isBusy: boolean = false;
 
-  forgotPassword() {
-    this.usernameValidated = this.username !== '';
-    if (this.usernameValidated) {
-      const userData = { username: this.username };
-      this.$store.dispatch(MutationTypes.FORGOT_PASSWORD_REQUEST, userData);
+  async forgotPassword() {
+    // Form Validation
+    this.error = {};
+    if (!this.userData.username) this.error.username = 'Please input your username.';
+    if (!_.isEmpty(this.error)) return;
+    
+    // Forgot password
+    this.isBusy = true;
+    const forgotPasswordResponse = await this.$store.dispatch(MutationTypes.FORGOT_PASSWORD_REQUEST, { username: this.userData.username });
+    if (forgotPasswordResponse.status === 'error') {
+      this.error.response = forgotPasswordResponse.msg;
+      this.error = { ...this.error };
+      this.isBusy = false;
+      return;
     }
+    this.step = 1;
+    this.isBusy = false;
   }
 
-  resetPassword() {
-    this.codeValidated = this.code !== '';
-    this.newPasswordValidated = this.newPassword && this.newPassword.length >= 6;
-    this.confirmPasswordValidated = this.confirmPassword === this.newPassword;
-    if (this.codeValidated && this.newPasswordValidated && this.confirmPasswordValidated) {
-      const userData = { username: this.username, code: this.code, newPassword: this.newPassword };
-      this.$store.dispatch(MutationTypes.CONFIRM_PASSWORD_REQUEST, userData);
+  async resetPassword() {
+    // Form Validation
+    this.error = {};
+    if (!this.userData.code) this.error.code = 'Please input the confirmation code.';
+    if (!this.userData.newPassword) this.error.newPassword = 'Please input new password.';
+    if (this.userData.newPassword !== this.userData.confirmPassword) this.error.confirmPassword = 'Password does not match.';
+    if (!_.isEmpty(this.error)) return;
+
+    // Reset password
+    this.isBusy = true;
+    const confirmPasswordResponse = await this.$store.dispatch(MutationTypes.CONFIRM_PASSWORD_REQUEST, {
+      username: this.userData.username,
+      code: this.userData.code,
+      newPassword: this.userData.newPassword,
+    });
+    if (confirmPasswordResponse.status === 'error') {
+      this.error.response = confirmPasswordResponse.msg;
+      this.error = { ...this.error };
+      this.isBusy = false;
+      return;
     }
+    this.step = 2;
+    this.isBusy = false;
   }
 }
