cdddf2daad0bc06d2d0ab65c7aae1f1eec3c6f18
Daniel Ho <dan1114ho@gmail.com>
2018-10-27 04:28:27 -0600

    Add dropdown filters

--- src/containers/CampaignRecommendation/CampaignRecommendation.ts
+++ src/containers/CampaignRecommendation/CampaignRecommendation.ts
@@ -17,8 +17,44 @@ declare function getSubjectId();
   },
   data: function() {
     return {
-      subjectId: getSubjectId()
+      subjectId: getSubjectId(),
+      adopt_curve_x: '',
+      adopt_curve_y: '',
+      adopt_curve_limit: 'top_1000',
+      adopt_curve_x_options: [],
+      adopt_curve_y_options: [],
+      adopt_curve_limit_options: [],
     };
+  },
+
+  created() {
+
+    function formatOptions(corpora) {
+      const rv = [];
+      corpora.forEach(function (corpus) {
+        rv.push(
+          { 'value': corpus[0], 'text': corpus[1], }
+        );
+      });
+      return rv;
+    }
+
+    this.$store.dispatch(MutationTypes.GET_CORPORA_REQUEST, {
+      payload: {}, callback: res => {
+        const corpora = res.data['client_corpora'].concat(res.data['wrench_corpora'], res.data['common_corpora']);
+        const dropdownValues = formatOptions(corpora);
+        this.$data.adopt_curve_x_options = dropdownValues;
+        this.$data.adopt_curve_y_options = dropdownValues;
+      }
+    });
+
+    this.$store.dispatch(MutationTypes.GET_CLIENT_FILE_FILTERS_REQUEST, {
+      payload: {}, callback: res => {
+        const filters = res.data;
+        const dropdownValues = formatOptions(filters);
+        this.$data.adopt_curve_limit_options = dropdownValues;
+      }
+    });
   }
 })
 
@@ -28,29 +64,36 @@ export class CampaignRecommendationContainer extends Vue {
   error: string = '';
   isBusy: boolean = false;
 
-  mounted() {
-    this.getContacts();
-  }
-
   async getContacts() {
-    this.isBusy = true;
-    /* const exportContactsResponse = await this.$store.dispatch(MutationTypes.EXPORT_CONTACTS_REQUEST, { campaign_recommendation: "prelaunch_camp_feedback" });
-    if (exportContactsResponse.status === 'error') {
-      this.error = exportContactsResponse.msg;
+    if (this.$data.adopt_curve_x !== '' && this.$data.adopt_curve_y !== '') {
+      this.isBusy = true;
+
+      const contactParams = {
+        campaign_recommendation: 'prelaunch_camp_feedback',
+        x: this.$data.adopt_curve_x,
+        y: this.$data.adopt_curve_y,
+        filter: this.$data.adopt_curve_limit
+      }
+      /* --- API is not ready ---
+      const exportContactsResponse = await this.$store.dispatch(MutationTypes.EXPORT_CONTACTS_REQUEST, contactParams);
+      if (exportContactsResponse.status === 'error') {
+        this.error = exportContactsResponse.msg;
+        this.isBusy = false;
+        return;
+      }
+      this.contactColumns = exportContactsResponse.payload.columns_display.map(label => ({
+        label,
+        filterable: true,
+        align: 'center'
+      }));
+      exportContactsResponse.payload.columns_keys.forEach((key, index) => {
+        this.contactColumns[index]['field'] = key;
+      });
+      this.contactData = exportContactsResponse.payload.data;
+      */
+      this.error = '';
       this.isBusy = false;
-      return;
     }
-    this.contactColumns = exportContactsResponse.payload.columns_display.map(label => ({
-      label,
-      filterable: true,
-      align: 'center'
-    }));
-    exportContactsResponse.payload.columns_keys.forEach((key, index) => {
-      this.contactColumns[index]['field'] = key;
-    });
-    this.contactData = exportContactsResponse.payload.data;*/
-    this.error = '';
-    this.isBusy = false;
   }
 
   onDownloadAsCSV() {
